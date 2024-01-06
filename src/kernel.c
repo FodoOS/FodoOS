e0099ff89cdebe673c7fad74ed79939bcf8d64d0
fedi-nabli <fedinabli@gmail.com>
2024-01-06 15:57:32 +0100

    Implemented a disk streamer

--- src/kernel.c
+++ src/kernel.c
@@ -8,6 +8,7 @@
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
 #include "disk/disk.h"
+#include "disk/streamer.h"
 #include "fs/pparser.h"
 
 uint16_t* video_mem = 0;
@@ -93,7 +94,9 @@ void kernel_main()
   // Enable the system interrupts
   enable_interrupts();
 
-  struct path_root* root_path = pathparser_parse("0:/bin/shell.exe");
-
-  if (root_path) {}
+  struct disk_stream* stream = diskstreamer_new(0);
+  diskstreamer_seek(stream, 0x201);
+  unsigned char c = 0;
+  diskstreamer_read(stream, &c, 1);
+  while (1) {}
 }
