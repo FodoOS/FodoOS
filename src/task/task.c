6fef17f6e3323415f28e2b39831884134831c8f3
fedi-nabli <fedinabli@gmail.com>
2024-01-15 16:08:22 +0100

    Fixed task bug

--- src/task/task.c
+++ src/task/task.c
@@ -38,8 +38,8 @@ struct task* task_new(struct process* process)
 
   if (task_head == 0)
   {
-    task_head = 0;
-    task_tail = 0;
+    task_head = task;
+    task_tail = task;
     current_task = task;
     goto out;
   }
@@ -139,7 +139,7 @@ int task_init(struct task* task, struct process* process)
   task->registers.ip = FODOOS_PROGRAM_VIRTUAL_ADDRESS;
   task->registers.ss = USER_DATA_SEGMENT;
   task->registers.cs = USER_CODE_SEGMENT;
-  task->registers.esp = FODOOS_PROGRAM_VIRTUAL_STACK_ADRESS_START;
+  task->registers.esp = FODOOS_PROGRAM_VIRTUAL_STACK_ADDRESS_START;
 
   task->process = process;
 
