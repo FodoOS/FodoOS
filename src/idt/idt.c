e7d7cbefdcc0304135c309c4245b0ec8d657dc95
fedi-nabli <fedinabli@gmail.com>
2024-01-28 18:17:49 +0100

    Creating a cleaner way to create interrupt handlers in the interrupt descriptor table

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -1,11 +1,13 @@
 #include "idt.h"
 #include "config.h"
+#include"status.h"
 #include "kernel.h"
 #include "io/io.h"
 #include "task/task.h"
 #include "memory/memory.h"
 
 static ISR80H_COMMAND isr80h_commands[FODOOS_MAX_ISR80H_COMMANDS];
+static INTERRUPT_CALLBACK_FUNCTION interrupt_callbacks[FODOOS_TOTAL_INTERRUPTS];
 
 struct idt_desc idt_descriptors[FODOOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
@@ -24,6 +26,13 @@ void no_interrupt_handler()
 
 void interrupt_handler(int interrupt, struct interrupt_frame* frame)
 {
+  kernel_page();
+  if (interrupt_callbacks[interrupt] != 0)
+  {
+    task_current_save_state(frame);
+    interrupt_callbacks[interrupt](frame);
+  }
+  task_page();
   outb(0x20, 0x20);
 }
 
@@ -58,6 +67,17 @@ void idt_init()
 
   // Load the interrupt descriptor table
   idt_load(&idtr_descriptor);
+}
+
+int idt_register_interrupt_callback(int interrupt, INTERRUPT_CALLBACK_FUNCTION interrupt_callback)
+{
+  if (interrupt < 0 || interrupt >= FODOOS_TOTAL_INTERRUPTS)
+  {
+    return -EINVARG;
+  }
+
+  interrupt_callbacks[interrupt] = interrupt_callback;
+  return 0;
 }
 
 void isr80h_register_command(int command_id, ISR80H_COMMAND command)
