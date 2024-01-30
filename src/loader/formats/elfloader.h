23aaf69b22855ed614de608fc2395505dc1745b1
fedi-nabli <fedinabli@gmail.com>
2024-01-30 19:58:38 +0100

    Implementing ELF Loader - Part 2

---
+++ src/loader/formats/elfloader.h
@@ -0,0 +1,42 @@
+#ifndef ELFLOADER_H
+#define ELFLOADER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "elf.h"
+#include "config.h"
+
+struct elf_file
+{
+  char filename[FODOOS_MAX_PATH];
+
+  int in_memory_size;
+
+  /**
+   * @brief The physical memory address that this elf file is loaded at
+   */
+  void* elf_memory;
+
+  /**
+   * @brief The virtual base address of this binary
+   */
+  void* virtual_base_address;
+
+  /**
+   * @brief The virtual end address of this binary
+   */
+  void* virtual_end_address;
+
+  /**
+   * @brief The physical base address of this binary
+   */
+  void* physical_base_address;
+
+  /**
+   * @brief The physical end address of this binary
+   */
+  void* physical_end_address;
+};
+
+#endif
