d04c2f178b382b065eaadb5bf3fb70ef6f4fc3ae
fedi-nabli <fedinabli@gmail.com>
2024-02-02 16:23:39 +0100

    Implementing system print in stdlib

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,4 +1,9 @@
+#include "fodoos.h"
+
 int main(int argc, char** argv)
 {
+  print("Hello, how are you!\n");
+
+  while (1) {}
   return 0;
 }
---
+++ programs/stdlib/src/fodoos.h
@@ -0,0 +1,6 @@
+#ifndef FODOOS_H
+#define FODOOS_H
+
+void print(const char* message);
+
+#endif
