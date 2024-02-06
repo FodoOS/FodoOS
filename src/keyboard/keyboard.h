d4ac41d6d4f18bdb7f28d6e3d8878336f8c7fad4
fedi-nabli <fedinabli@gmail.com>
2024-02-06 23:08:15 +0100

    Handling caps lock

--- src/keyboard/keyboard.h
+++ src/keyboard/keyboard.h
@@ -1,6 +1,11 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#define KEYBOARD_CAPS_LOCK_ON 1
+#define KEYBOARD_CAPS_LOCK_OFF 0
+
+typedef int KEYBOARD_CAPS_LOCK_STATE;
+
 struct process;
 
 typedef int (*KAYBOARD_INIT_FUNCTION)();
@@ -9,12 +14,17 @@ struct keyboard
 {
   KAYBOARD_INIT_FUNCTION init;
   char name[20];
+
+  KEYBOARD_CAPS_LOCK_STATE capslock_state;
+
   struct keyboard* next;
 };
 
 void keyboard_init();
 int keyboard_insert(struct keyboard* keyboard);
 void keyboard_backspace(struct process* process);
+void keyboard_set_capslock(struct keyboard* keyboard, KEYBOARD_CAPS_LOCK_STATE state);
+KEYBOARD_CAPS_LOCK_STATE keyboard_get_capslock(struct keyboard* keyboard);
 void keyboard_push(char c);
 char keyboard_pop();
 
